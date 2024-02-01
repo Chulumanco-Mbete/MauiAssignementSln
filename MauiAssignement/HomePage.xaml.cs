c29ef9194b555b404348c66a20cfa928635e20b4
Chulumanco-Mbete <5100151@mypillar7uwc.ac.za>
2024-02-01 15:07:45 +0200

    Code Updated

--- MauiAssignement/HomePage.xaml.cs
+++ MauiAssignement/HomePage.xaml.cs
@@ -8,19 +8,66 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
         Routing.RegisterRoute("SearchPage", typeof(SearchPage));
-	}
+
+    }
 
     private async void SearchButton_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("SearchPage");
     }
-
-    private void Ad_1_Button_Clicked(object sender, EventArgs e)
+    private async void Ad_1_Button_Clicked(object sender, EventArgs e)
     {
-        WebView webvView = new WebView
-        {
-            Source = "https://www.fxpro.com/"
-        };
+        await Launcher.OpenAsync("https://www.fxpro.com/");
+    }
+    private async void Video_1_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/Kt3EmcyS_N4");
+    }
+    private async void Video_12_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/UQ4VeJeDTH0");
+    }
+    private async void Video_13_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/ArhhQJpel-c");
+    }
+    private async void Ad_2_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://play.google.com/store/apps/details?id=com.king.candycrushsaga");
     }
 
+    private async void Video_4_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/fFhH-pt9JW8");
+    }
+    private async void Video_17_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/sXOsmFED_9I");
+    }
+    private async void Video_14_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/MvPp-LF7Rlo");
+    }
+
+    private async void Ad_3_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://preply.com/");
+    }
+
+    private async void Video_15_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/EiUIMFFCVLs");
+    }
+    private async void Video_5_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/9ZQqq_mGYSM");
+    }
+    private async void Video_8_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/KMmKPfYhv20");
+    }
+    private async void Ad_4_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://www.booking.com/");
+    }
 }
--- MauiAssignement/SubscriptionPage.xaml.cs
+++ MauiAssignement/SubscriptionPage.xaml.cs
@@ -5,5 +5,66 @@ public partial class SubscriptionPage : ContentPage
 	public SubscriptionPage()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute("SearchPage", typeof(SearchPage));
+    }
+    private async void SearchButton_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("SearchPage");
+    }
+
+    private async void Ad_5_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://www.samsung.com/za/");
+    }
+    private async void Video_16_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/ElgoBqm7wFk");
+    }
+    private async void Video_6_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/veZOrXVHf7U");
+    }
+    private async void Video_9_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/GqulwE_yKww");
+    }
+    private async void Ad_6_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://za.shein.com/");
+    }
+
+    private async void Video_18_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/hH0mh0nrWO0");
+    }
+    private async void Video_2_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/UjmUhet1qW8");
+    }
+    private async void Video_3_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/NFh8ry3a9oY");
+    }
+
+    private async void Ad_7_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://www.betterhelp.com/");
+    }
+
+    private async void Video_11_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/DPjPcxp_dWs");
+    }
+    private async void Video_7_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/ErOzKJz6fo0");
+    }
+    private async void Video_10_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://youtu.be/4JdcO4UJkog");
+    }
+    private async void Ad_8_Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.OpenAsync("https://www.onecart.co.za/");
+    }
 }
